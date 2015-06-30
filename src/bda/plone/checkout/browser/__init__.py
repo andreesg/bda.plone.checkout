8b09a42b458d81810542008e004c9a2832c6c7f3
André Gonçalves <andreslb1@gmail.com>
2015-06-30 10:40:08 +0200

    Add header to all steps

--- src/bda/plone/checkout/browser/__init__.py
+++ src/bda/plone/checkout/browser/__init__.py
@@ -1,1 +1,18 @@
-#
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+from Products.Five import BrowserView
+
+
+class CheckoutView(BrowserView):
+    def get_tickets_header(self, is_ticket):
+        if is_ticket:
+            folder = self.context
+            if folder.portal_type == "Folder":
+                contents = folder.getFolderContents({"portal_type": "Image", "Title":"tickets-header"})
+                if len(contents) > 0:
+                    image = contents[0]
+                    url = image.getURL()
+                    scale_url = "%s/%s" %(url, "@@images/image/large")
+                    return scale_url
+        else:
+            return False
