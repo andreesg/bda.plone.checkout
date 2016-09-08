e205859a1d5892aebf3e0c407453babd5756c8e2
Andre Goncalves <andreslb1@gmail.com>
2016-09-08 14:21:33 +0200

    Implement tickets for any context that contains tickets on its contents

--- src/bda/plone/checkout/browser/__init__.py
+++ src/bda/plone/checkout/browser/__init__.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 from Products.Five import BrowserView
-
+from bda.plone.shop.utils import is_ticket as is_context_ticket
+from plone.app.uuid.utils import uuidToCatalogBrain
 
 class CheckoutView(BrowserView):
-    def get_tickets_header(self, is_ticket):
-        if is_ticket:
+
+    def is_ticket(self):
+        result = is_context_ticket(self.context)
+        return result
+
+    def get_tickets_header(self, ticket):
+        if ticket:
             folder = self.context
-            if folder.portal_type == "Folder":
-                contents = folder.getFolderContents({"portal_type": "Image", "Title":"tickets-header"})
-                if len(contents) > 0:
-                    image = contents[0]
-                    url = image.getURL()
-                    scale_url = "%s/%s" %(url, "@@images/image/large")
-                    return scale_url
+            if folder.portal_type in ["Folder", "Event"]:
+                if folder.portal_type == "Event":
+                    uuid = folder.UID()
+                    brain = uuidToCatalogBrain(uuid)
+                    if brain:
+                        leadmedia = getattr(brain, 'leadMedia', None)
+                        if leadmedia:
+                            image = uuidToCatalogBrain(leadmedia)
+                            if hasattr(image, 'getURL'):
+                                url = image.getURL()
+                                scale_url = "%s/%s" %(url, "@@images/image/large")
+                                return scale_url
+                else:
+                    contents = folder.getFolderContents({"portal_type": "Image", "Title":"tickets-header"})
+                    if len(contents) > 0:
+                        image = contents[0]
+                        url = image.getURL()
+                        scale_url = "%s/%s" %(url, "@@images/image/large")
+                        return scale_url
         else:
             brains = self.context.portal_catalog(Title="webwinkel-header", portal_type="Image")
             if len(brains) > 0:
