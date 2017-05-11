340b80a2b35866ae4d8275bfb950d67909c5f5d9
andreesg <andreslb1@gmail.com>
2017-05-11 10:50:48 +0200

    Add bank selection

--- src/bda/plone/checkout/browser/form.py
+++ src/bda/plone/checkout/browser/form.py
@@ -375,7 +375,7 @@ class BankSelection(FieldsProvider):
         for bank in self.banks:
             vocab.append((bank['id'], bank['name']))
         
-        vocab.insert(0, ('', _(u'choose_bank_label', default="Kies uw bank")))
+        vocab.insert(0, ('', _(u'choose_bank_label', default="Kies je bank")))
 
         return vocab
 
@@ -533,9 +533,20 @@ class CheckoutForm(Form, FormContext):
             self.finish_redirect_url = \
                 checkout_settings.skip_payment_redirect_url(uid)
         else:
-            p_name = data.fetch('checkout.payment_selection.payment').extracted
-            payments = Payments(self.context)
-            payment = payments.get(p_name)
-            self.finish_redirect_url = payment.init_url(str(uid))
+            try:
+                p_name = data.fetch('checkout.payment_selection.payment').extracted
+                bank_id = data.fetch('checkout.bank_selection.bank').extracted
+                payment_method = data.fetch('checkout.payment_method_selection.payment_method').extracted
+                payments = Payments(self.context)
+                payment = payments.get(p_name)
+                self.finish_redirect_url = payment.init_url(str(uid), str(bank_id), str(payment_method))
+            except:
+                raise
+                p_name = data.fetch('checkout.payment_selection.payment').extracted
+                payments = Payments(self.context)
+                payment = payments.get(p_name)
+                self.finish_redirect_url = payment.init_url(str(uid))
+
+
         event = CheckoutDone(self.context, self.request, uid)
         notify(event)
